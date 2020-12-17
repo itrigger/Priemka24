df00470d7daf505d033827f9492a0378b7888b1d
DESKTOP-N9C8HF0\Konstantin <trigger.kst@gmail.com>
2020-12-17 23:07:28 +0600

    imgs
    Took 26 minutes

--- src/sw-register.js
+++ src/sw-register.js
@@ -1,3 +1,5 @@
-navigator.serviceWorker
-  .register('/service-worker.js')
-  .catch((err) => console.error(err))
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/service-worker.js')
+        .catch((err) => console.error(err))
+}
